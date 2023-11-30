76cb79b50f77d605c833f0735bd55d7b7b388165
Abhishek Agarwal <abhishek.h.agarwal@oracle.com>
2023-11-30 19:34:44 +0530

    trying to make md to html script work as header in md files

--- TheAgarwalBlog/assets/js/static-md-2-html.js
+++ TheAgarwalBlog/assets/js/static-md-2-html.js
@@ -1,6 +1,8 @@
 
-console.log("Yolo");
-const text = document.body.innerHTML;
-var converter = new showdown.Converter();
-let mdText = converter.makeHtml(text);
-document.body.innerHTML = mdText;
+document.onload = () => {
+    console.log("Yolo");
+    const text = document.body.innerHTML;
+    var converter = new showdown.Converter();
+    let mdText = converter.makeHtml(text);
+    document.body.innerHTML = mdText;
+}
